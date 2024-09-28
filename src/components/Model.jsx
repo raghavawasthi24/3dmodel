d7e60c2aef04e7578978ce61757cb30f3a2b997b
raghavawasthi24 <raghav.a@sustainiam.com>
2024-09-28 14:11:35 +0530

    added 3d model

---
+++ src/components/Model.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useGLTF } from '@react-three/drei'
+
+export function Model(props) {
+  const { nodes, materials } = useGLTF("/Model/model.gltf");
+  return (
+    <group {...props} dispose={null}>
+      <group scale={0.01}>
+        <mesh geometry={nodes.Wood_Wood_0.geometry} material={materials.Wood} />
+        <mesh geometry={nodes.Wall_Wall_0.geometry} material={materials.Wall} />
+        <mesh geometry={nodes.WallBrick_Wall_Brick_0.geometry} material={materials.Wall_Brick} />
+        <mesh geometry={nodes.Roof_Roof_0.geometry} material={materials.Roof} />
+        <mesh geometry={nodes.DoorAndWindow_Door_And_Window_0.geometry} material={materials.Door_And_Window} />
+      </group>
+    </group>
+  )
+}
+
+useGLTF.preload('/Model/model.gltf')
